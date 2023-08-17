63d8848e160bb61779daec027bfc0c8ca64f958a
Mohamed Ahmed (Laptop) <mohamedtaha2005@gmail.com>
2023-08-17 12:07:36 +0200

    Deleted files

--- password_generator/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class RequestForm(forms.Form):
-    length = forms.IntegerField()
-    symbols = forms.BooleanField(required=False)
-    numbers = forms.BooleanField(required=False)
